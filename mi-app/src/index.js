e8d56c69798391fdabea11ceba58b0e4857abf1c
Luis Boville Lobo <luisbovillelobo@users.noreply.github.com>
2017-05-16 12:36:48 +0200

    Add files via upload

---
+++ mi-app/src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Boton from './menu';
+import './index.css';
+import './semantic.min.css'
+
+ReactDOM.render(
+	<div>
+	  	<Boton />
+  	</div>,
+  document.getElementById('window')
+);
---
+++ mi-app/src/menu.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Boton extends Component {
+    render() {
+        return (
+            <div>
+                <a className="ui primary button" href="">Pago inmediato</a>
+            </div>
+        )
+    } 
+}
+
+export default Boton;
